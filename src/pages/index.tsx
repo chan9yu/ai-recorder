2a77b6190d8df2072702a9fc340967e398757b33
chan9yu <dev.cgyeo@gmail.com>
2025-02-13 12:38:42 +0900

    chore: pretendard 폰트 추가, 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,26 @@
-import { Geist, Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin']
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin']
+const pretendard = localFont({
+	src: '../assets/fonts/PretendardVariable.woff2',
+	display: 'swap',
+	weight: '45 920',
+	variable: '--font-pretendard'
 });
 
 export default function HomePage() {
 	return (
-		<div className={`${geistSans.variable} ${geistMono.variable}`}>
-			<div className="w-screen h-screen flex items-center justify-center">Hello World!</div>
+		<div className={pretendard.variable}>
+			<div className="w-screen h-screen flex flex-col items-center justify-center gap-4 font-pretendard">
+				<span className="font-thin">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+				<span className="font-extralight">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+				<span className="font-light">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+				<span className="font-normal">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+				<span className="font-medium">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+				<span className="font-semibold">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+				<span className="font-bold">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+				<span className="font-extrabold">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+				<span className="font-black">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+			</div>
 		</div>
 	);
 }
